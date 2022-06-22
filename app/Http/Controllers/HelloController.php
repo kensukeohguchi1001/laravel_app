7d77b5b868c34f5076ab1d37b50a5303eef15802
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-06-23 06:58:12 +0900

    ルートとモデルの結合

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Person;
+
+class HelloController extends Controller
+{
+    public function index(Person $person)
+    {
+        $data = [
+            'msg' => $person,
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function other (Request $request)
+    {
+        $data = [
+            'msg' => $request->bye
+        ];
+        return view('hello.index', $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{person}', 'App\Http\Controllers\HelloController@index');
